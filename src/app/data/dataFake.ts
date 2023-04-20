3c662c4ca22fd9b5984ada2ad69d77eb98c617fa
rafael <rafaeljsf98@outlook.com>
2023-04-20 18:39:03 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id" : "1",
+    "photoCover": "https://avancegames.com.br/wp-content/uploads/2023/04/demon-slayer-Kimetsu-no-Yaiba-temporada-3_avancegames-696x464.webp",
+    "title": "Detalhes da nova temporada de Kimetsu no Yaiba",
+    "description": "Leia aqui as notícias mais recentes e detalhes do anime que está ganhando legiões de fãs por todo o mundo!"
+  },
+  {
+    "id" : "2",
+    "photoCover": "https://sm.ign.com/ign_br/screenshot/default/jujutsu-kaisen-temporada-2-data-estreia-onde-assistir-person_13xs.jpg",
+    "title": "Acompanhe novas notícias de Jujutsu Kaisen",
+    "description": "O tão apreciado anime dos fãs de Satoru Gojo já possui novidades a serem discutidas. Leia logo abaixo o que já se sabe!"
+  },
+  {
+    "id" : "3",
+    "photoCover": "https://cdn.myanimelist.net/images/anime/1000/110531.jpg",
+    "title": "A última temporada de Shingeki no Kyojin",
+    "description": "A última temporada dos temidos titãs rendeu diversas opiniões entre os fãs do anime. Separamos aqui algumas delas!"
+  },
+  {
+    "id" : "4",
+    "photoCover": "https://www.slashfilm.com/img/gallery/hunter-x-hunter-season-7-is-it-ever-going-to-happen/intro-1645028556.jpg",
+    "title": "Mais aventuras do Gon?",
+    "description": "Leia aqui o que sabemos a respeito da possibilidade da nova temporada de Hunter x Hunter!"
+  }
+];
